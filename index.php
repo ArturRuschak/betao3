54ad63056061504415791269277ae7a063f60cb9
Artur Ruschak <ruschakartur@gmail.com>
2018-04-07 11:25:32 +0300

    jaja

--- index.php
+++ index.php
@@ -1,86 +1,49 @@
 <?php
-
-
 $continents =[
-
-       "africa" =>  ["Elephant","African Hare","Elephant Shrew"],
-       "europe" => ["Wild","Karakurt Spider","Norwegian Lemmin"],
-        "asia"=> ["Malayan Tapir","Indian","Slow Loris"],
-        "australia" => ["Cane Toad","Black Swan","Taipan"],
-       "antarctica" =>["Whale","Emperor penguins","Adelie penguin"]
-
-
+    "africa" =>  ["Elephant","African Hare","Elephant Shrew"],
+    "europe" => ["Wild","Karakurt Spider","Norwegian Lemmin"],
+    "asia"=> ["Malayan Tapir","Indian","Slow Loris"],
+    "australia" => ["Cane Toad","Black Swan","Taipan"],
+    "antarctica" =>["Whale","Emperor penguins","Adelie penguin"]
 ];
-
-
-
 foreach ($continents as $key => $value ) {
     echo "<h1>", $key, ':', "</h1>", "<br/>";
     foreach ($value as $item) {
         $count = $item;
         $animals = str_word_count($item);
         if ($animals == 2) {
-            echo $item, "<br/>";
+            //echo $item, "<br/>";
             $array[] = $item;
+            $all_animals = explode(' ', $item, 2);
+            $first_animals[] = $all_animals[0];
+            $second_animals[] = $all_animals[1];
+
+            shuffle($first_animals);
+
+            shuffle($second_animals);
+
+            foreach ($first_animals as $an) {
+                echo $an, ' ', "<br>";
+        break;
+            }
+            foreach ($second_animals as $value) {
+
+                echo $value , ' ', PHP_EOL;
+            break;
+
+            }
+
         }
     }
 }
 echo "<pre>";
-var_dump($array);
+var_dump($first_animals);
+var_dump($second_animals);
 
-
-
-
-
+//echo "<pre>";
+//var_dump($array);
 /*
-if($continents) {
-
-
-
-
-}
-
-else {
-
-}
-
-
-
-
-
-
-
-/*
-PHP_EOL;
-echo "<pre>";
-var_dump($continents);
-echo "</pre>";
-foreach ($continents as $key => $value) {
-
-        echo $key , "<br/>", PHP_EOL ;
-        foreach($value as $item){
-
-            echo $item , "<br/>";
-
-
-        }
-
-    }
-echo '////////////////////////////////////////////////////////////////';
-
-echo "<br/>";
-*/
-
-
-
-
-/*
-echo "<pre>";
-var_dump($doublewords);
-echo "</pre>";
-
-echo $doublewords[rand(1,2)];*/
-
-
-
-?>
+for($i=0;$i<2;$i++) {
+    echo $array[$i];
+    break;
+}*/
